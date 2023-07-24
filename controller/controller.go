88be6d348246ec477022e6e8db03f84fe96a614f
b10930032@ms.ntust.edu.tw <b10930032@ms.ntust.edu.tw>
2023-07-24 18:05:56 +0800

    feat: post and get detector response

--- controller/controller.go
+++ controller/controller.go
@@ -1,15 +1,25 @@
 package controller
 
 import (
+	"MDEP/services"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetDetectorList(c *gin.Context) {
-
+	projection := bson.D{{"detector_id", 1}, {"detector_name", 1}}
+	results := services.MongoClient.ListDetector("MDEP", "detector", projection)
+	var response []bson.M
+	for _, res := range results {
+		response = append(response, bson.M{"detector_id": res.Id.Hex(), "detector_name": res.Name})
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 func CreateDetector(c *gin.Context) {
-
+	c.JSON(http.StatusNoContent, gin.H{})
 }
 
 func CreateTask(c *gin.Context) {
--- services/databaseService.go
+++ services/databaseService.go
@@ -2,12 +2,15 @@ package services
 
 import (
 	"context"
+	"log"
+	"os"
+
 	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
-	"os"
 )
 
 type MongoDBClient struct {
@@ -15,7 +18,26 @@ type MongoDBClient struct {
 	ctx    context.Context
 }
 
-var mongoClient = NewMongoDBClient(os.Getenv("MONGODB_URI"))
+type DetectorRes struct {
+	Id   primitive.ObjectID `json:"detector_id" bson:"_id,omitempty"`
+	Name string             `json:"detector_name" bson:"name,omitempty"`
+}
+
+type ReportRes struct {
+	Id             primitive.ObjectID `json:"detector_id" bson:"_id,omitempty"`
+	FuncType       string             `json:"function_type" bson:"function_type,omitempty"`
+	Accuracy       float64            `json:"accuracy" bson:"accuracy,omitempty"`
+	FP             float64            `json:"fp" bson:"fp,omitempty"`
+	FN             float64            `json:"fn" bson:"fn,omitempty"`
+	Precision      float64            `json:"precision" bson:"precision,omitempty"`
+	Recall         float64            `json:"recall" bson:"recall,omitempty"`
+	F1             float64            `json:"f1" bson:"f1,omitempty"`
+	TestTime       float64            `json:"testing_time" bson:"testing_time,omitempty"`
+	TestSampleRun  float64            `json:"testing_sample_run" bson:"testing_sample_run,omitempty"`
+	TotalSampleRun float64            `json:"total_sample_run" bson:"total_sample_run,omitempty"`
+}
+
+var MongoClient = NewMongoDBClient(os.Getenv("MONGODB_URI"))
 
 func NewMongoDBClient(uri string) *MongoDBClient {
 	client, ctx, err := ConnectMongo(uri)
@@ -54,4 +76,21 @@ func PingMongo(client *mongo.Client, ctx context.Context) error {
 	}
 	log.Println("connected successfully")
 	return nil
+}
+
+func (MongoClient *MongoDBClient) ListDetector(databaseName string, collectionName string, projection bson.D) []DetectorRes {
+	collection := MongoClient.client.Database(databaseName).Collection(collectionName)
+	opts := options.Find().SetProjection(projection)
+	cursor, err := collection.Find(MongoClient.ctx, opts)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	var results []DetectorRes
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return results
 }

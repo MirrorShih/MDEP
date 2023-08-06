301a2972f57533cf25070324297ba370fc5f9ec7
MirrorShih <jess2529156651@gmail.com>
2023-08-06 17:28:43 +0800

    feat: add run detector function

--- controller/controller.go
+++ controller/controller.go
@@ -3,12 +3,15 @@ package controller
 import (
 	"MDEP/models"
 	"MDEP/services"
+	"encoding/csv"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 func GetDetectorList(c *gin.Context) {
@@ -39,8 +42,61 @@ func CreateDetector(c *gin.Context) {
 	c.JSON(http.StatusNoContent, gin.H{})
 }
 
+type TaskRequest struct {
+	DetectorId   string `json:"detector_id"`
+	FunctionType string `json:"function_type"`
+}
+
 func CreateTask(c *gin.Context) {
 	// TODO
+	var json TaskRequest
+	c.BindJSON(&json)
+	id, _ := primitive.ObjectIDFromHex(json.DetectorId)
+	log.Println(json.DetectorId)
+	log.Printf("%v", &json)
+	filter := bson.D{bson.E{Key: "_id", Value: id}}
+	results := services.MongoClient.GetCertainDetector("MDEP", "detector", filter)
+	taskPath := "/home/MDEP/task/"
+	os.Mkdir(taskPath, os.ModePerm)
+	defer os.RemoveAll(taskPath)
+	services.MongoClient.DownloadFile("MDEP", results.FileId, taskPath+"detector.zip")
+	cmd := exec.Command("unzip", taskPath+"detector.zip", "-d", taskPath)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("cmd.Run() failed with %s\n", err)
+	}
+	cmd = exec.Command("python", "scripts/envBuilder.py", taskPath+"taskEnv")
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("cmd.Run() failed with %s\n", err)
+	}
+	cmd = exec.Command(taskPath+"taskEnv/bin/pip", "install", "-r", taskPath+"requirements.txt")
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("cmd.Run() failed with %s\n", err)
+	}
+	cmd = exec.Command("taskEnv/bin/python", "main.py", "-i", "TestingBin/0000dc2f3c8bde2d3b61cd1ba3aa5e839c0a7bf432d2e06a88a7ce3b199453e7", "-o", "myDetector_FC_records.csv", "-c")
+	cmd.Dir = taskPath
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("cmd.Run() failed with %s\n", err)
+	}
+	content, err := os.OpenFile(taskPath+"myDetector_FC_records.csv", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalln("Cannot find csv file:", taskPath+"myDetector_FC_records.csv", err)
+	}
+	r := csv.NewReader(content)
+	r.Comma = ','
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("%v\n", record)
+	}
 	c.JSON(http.StatusNoContent, gin.H{})
 }
 

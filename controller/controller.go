6e88387933f383f37f688f296389411cccd75365
MirrorShih <jess2529156651@gmail.com>
2023-08-22 17:58:17 +0800

    feat: add run task feature

--- controller/controller.go
+++ controller/controller.go
@@ -6,11 +6,18 @@ import (
 	"context"
 	"encoding/csv"
 	"encoding/json"
+	"github.com/Jeffail/tunny"
 	"io"
+	"io/fs"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
@@ -19,6 +26,25 @@ import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
+
+var pool *tunny.Pool
+
+type DetectorTask struct {
+	id        primitive.ObjectID
+	taskPath  string
+	functions []string
+	reportId  []string
+}
+
+func init() {
+	numCPUs := runtime.NumCPU()
+	pool = tunny.NewFunc(numCPUs, func(task interface{}) interface{} {
+		detectorTask := task.(DetectorTask)
+		RunTask(detectorTask.id, detectorTask.taskPath, detectorTask.functions, detectorTask.reportId)
+		return true
+	})
+	pool.SetSize(1)
+}
 
 type AuthController struct {
 	oauthConfig *oauth2.Config
@@ -109,23 +135,17 @@ func CreateDetector(c *gin.Context) {
 }
 
 type TaskRequest struct {
-	DetectorId   string `json:"detector_id"`
-	FunctionType string `json:"function_type"`
+	DetectorId   string   `json:"detector_id"`
+	FunctionType []string `json:"function_type"`
 }
 
-func CreateTask(c *gin.Context) {
-	// TODO
-	var json TaskRequest
-	c.BindJSON(&json)
-	id, _ := primitive.ObjectIDFromHex(json.DetectorId)
-	log.Println(json.DetectorId)
-	log.Printf("%v", &json)
-	filter := bson.D{bson.E{Key: "_id", Value: id}}
+func DownloadDetector(detectorId primitive.ObjectID, taskPath string) {
+	filter := bson.D{bson.E{Key: "_id", Value: detectorId}}
 	results := services.MongoClient.GetCertainDetector("MDEP", "detector", filter)
-	taskPath := "/home/MDEP/task/"
-	os.Mkdir(taskPath, os.ModePerm)
-	defer os.RemoveAll(taskPath)
 	services.MongoClient.DownloadFile("MDEP", results.FileId, taskPath+"detector.zip")
+}
+
+func InitDetector(taskPath string) {
 	cmd := exec.Command("unzip", taskPath+"detector.zip", "-d", taskPath)
 	err := cmd.Run()
 	if err != nil {
@@ -141,29 +161,117 @@ func CreateTask(c *gin.Context) {
 	if err != nil {
 		log.Printf("cmd.Run() failed with %s\n", err)
 	}
-	cmd = exec.Command("taskEnv/bin/python", "main.py", "-i", "TestingBin/0000dc2f3c8bde2d3b61cd1ba3aa5e839c0a7bf432d2e06a88a7ce3b199453e7", "-o", "myDetector_FC_records.csv", "-c")
-	cmd.Dir = taskPath
+	cmd = exec.Command("cp", "/src/scripts/metrics.py", taskPath)
 	err = cmd.Run()
 	if err != nil {
 		log.Printf("cmd.Run() failed with %s\n", err)
 	}
-	content, err := os.OpenFile(taskPath+"myDetector_FC_records.csv", os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		log.Fatalln("Cannot find csv file:", taskPath+"myDetector_FC_records.csv", err)
-	}
-	r := csv.NewReader(content)
-	r.Comma = ','
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
+}
+
+func RunDetector(taskPath string, functions, reportId []string) {
+	datasetPath := "/mnt/dataset/"
+	for i, function := range functions {
+		startTime := time.Now()
+		minTime := -1.
+		maxTime := -1.
+		totalNum := 0.0
+		err := filepath.Walk(datasetPath+function, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if info.Name() == "dataset.csv" {
+				return nil
+			}
+			totalNum += 1
+			startFileTime := time.Now()
+			cmd := exec.Command("taskEnv/bin/python", "main.py", "-i", path, "-o", function+"records.csv", "-c")
+			cmd.Dir = taskPath
+			err = cmd.Run()
+			if err != nil {
+				log.Printf("cmd.Run() failed with %s\n", err)
+				return err
+			}
+			executionTime := time.Since(startFileTime).Seconds()
+			if minTime == -1 {
+				minTime = executionTime
+				maxTime = executionTime
+			} else {
+				minTime = math.Min(minTime, executionTime)
+				maxTime = math.Max(maxTime, executionTime)
+			}
+			return nil
+		})
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
-		log.Printf("%v\n", record)
+		testingTime := time.Since(startTime).Seconds()
+		cmd := exec.Command("python", taskPath+"metrics.py", function)
+		cmd.Dir = taskPath
+		err = cmd.Run()
+		if err != nil {
+			log.Printf("cmd.Run() failed with %s\n", err)
+		}
+		reportID, err := primitive.ObjectIDFromHex(reportId[i])
+		if err != nil {
+			log.Printf("cannot covert report id")
+		}
+		content, err := os.OpenFile(taskPath+"metrics.csv", os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			log.Println("Cannot find csv file:", taskPath+"metrics.csv", err)
+		}
+		r := csv.NewReader(content)
+		title := true
+		var accuracy float64
+		var testSampleNum float64
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Println(err)
+			}
+			if title {
+				title = false
+				continue
+			}
+			testSampleNum, err = strconv.ParseFloat(record[0], 64)
+			if err != nil {
+				log.Println(err)
+			}
+			accuracy, err = strconv.ParseFloat(record[1], 64)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		services.MongoClient.InsertReport("MDEP", "report", models.Report{reportID, function, accuracy, 0, 0, 0, 0, 0, testingTime / totalNum, minTime, maxTime, testingTime, testSampleNum, totalNum})
 	}
-	c.JSON(http.StatusNoContent, gin.H{})
+}
+
+func RunTask(detectorId primitive.ObjectID, taskPath string, functions, reportId []string) {
+	DownloadDetector(detectorId, taskPath)
+	InitDetector(taskPath)
+	RunDetector(taskPath, functions, reportId)
+	os.RemoveAll(taskPath)
+}
+
+func CreateTask(c *gin.Context) {
+	var json TaskRequest
+	c.BindJSON(&json)
+	id, _ := primitive.ObjectIDFromHex(json.DetectorId)
+	log.Println(json.DetectorId)
+	log.Printf("%v", &json)
+	taskPath := "/home/MDEP/task/"
+	os.Mkdir(taskPath, os.ModePerm)
+	var reportID []string
+	for _ = range json.FunctionType {
+		reportID = append(reportID, primitive.NewObjectID().Hex())
+	}
+	go pool.Process(DetectorTask{id, taskPath, json.FunctionType, reportID})
+	c.JSON(http.StatusOK, gin.H{"report_id": reportID})
 }
 
 func GetReportList(c *gin.Context) {

c618a1f49b5a61349362c71dcb22ed8a654cc146
eeechun <tammy60327@gmail.com>
2023-09-22 12:59:12 +0800

    feat: group api

--- router/router.go
+++ router/router.go
@@ -2,55 +2,58 @@ package router
 
 import (
 	"MDEP/controller"
+	"MDEP/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Route struct {
-	Method  func(engine *gin.Engine, path string, handler func(c *gin.Context))
+	Method  func(engine *gin.RouterGroup, path string, handler func(c *gin.Context))
 	Path    string
 	Handler func(c *gin.Context)
 }
 
 var routes []Route
 
-func GET(engine *gin.Engine, path string, handler func(c *gin.Context)) {
+func GET(engine *gin.RouterGroup, path string, handler func(c *gin.Context)) {
 	engine.GET(path, handler)
 }
 
-func POST(engine *gin.Engine, path string, handler func(c *gin.Context)) {
+func POST(engine *gin.RouterGroup, path string, handler func(c *gin.Context)) {
 	engine.POST(path, handler)
 }
 
-func PUT(engine *gin.Engine, path string, handler func(c *gin.Context)) {
+func PUT(engine *gin.RouterGroup, path string, handler func(c *gin.Context)) {
 	engine.PUT(path, handler)
 }
 
-func DELETE(engine *gin.Engine, path string, handler func(c *gin.Context)) {
+func DELETE(engine *gin.RouterGroup, path string, handler func(c *gin.Context)) {
 	engine.DELETE(path, handler)
 }
 
-func register(method func(engine *gin.Engine, path string, handler func(c *gin.Context)), path string, handler func(c *gin.Context)) {
+func register(method func(engine *gin.RouterGroup, path string, handler func(c *gin.Context)), path string, handler func(c *gin.Context)) {
 	routes = append(routes, Route{method, path, handler})
 }
 
 func init() {
-	register(GET, "/auth/callback", controller.HandleCallback)
-
-	register(GET, "/api/detector", controller.GetDetectorList)
-	register(GET, "/api/detector/:id", controller.GetDetector)
-	register(POST, "/api/detector", controller.CreateDetector)
-	register(POST, "/api/task", controller.CreateTask)
-	register(GET, "/api/report", controller.GetReportList)
-	register(GET, "/api/report/:id", controller.GetReport)
-	register(PUT, "/api/detector/:id", controller.UpdateDetector)
-	register(DELETE, "/api/detector/:id", controller.DeleteDetector)
+	register(GET, "/detector", controller.GetDetectorList)
+	register(GET, "/detector/:id", controller.GetDetector)
+	register(POST, "/detector", controller.CreateDetector)
+	register(POST, "/task", controller.CreateTask)
+	register(GET, "/report", controller.GetReportList)
+	register(GET, "/report/:id", controller.GetReport)
+	register(PUT, "/detector/:id", controller.UpdateDetector)
+	register(DELETE, "/detector/:id", controller.DeleteDetector)
 }
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
+	routerGroup := router.Group("/api")
+	routerGroup.Use(middleware.GitHubAPIMiddleware)
+
 	for _, route := range routes {
-		route.Method(router, route.Path, route.Handler)
+		route.Method(routerGroup, route.Path, route.Handler)
 	}
+	router.GET("/auth/callback", controller.HandleCallback)
 	return router
 }
